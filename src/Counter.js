b3195085723954bef77255f77f20c9cb083cf054
Jeremy Kale Padot <jeremy@padot.us>
2020-04-13 14:49:45 -0700

    pass test WITH refactored functional method
    Co-authored-by: Drake Wilcox <drakewilcox@gmail.com>
    Co-authored-by: Dominique Youmans <46074403+21-Coding@users.noreply.github.com>
    Co-authored-by: Dusty McCord <dustymccord@gmail.com>

--- src/Counter.js
+++ src/Counter.js
@@ -1,49 +1,29 @@
 export class Counter {
-  construtor() {}
-  //470
-  getChange(amount, result = [ 0, 0, 0, 0 ], counter = 0) {
+  getChange(startingAmount) {
     const coinValues = [ 500, 100, 25, 10, 5, 1 ];
-    if (amount <= 0) {
-      console.log(result);
-      return result;
-    } else if (amount >= coinValues[counter]) {
-      const currentTotal = (amount -= coinValues[counter]);
-      // const addCoin = 5;
-      result[counter] = coinValues[counter];
-      const addCoin = result.concat(coinValues[counter]);
-      return this.getChange(currentTotal, result, counter + 1);
-    }
-    console.log(result);
-    return result;
 
-    // let thisResult = [];
+    const buildChange = (amount, index = 0, result = []) => {
+      if (amount > 0) {
+        if (amount >= coinValues[index]) {
+          const newTotal = (amount -= coinValues[index]);
 
-    // const changeBot = () => {
-    //   if (amount >= 500) {
-    //     thisResult.push(500);
-    //     changeBot((amount -= 500));
-    //   } else if (amount >= 100) {
-    //     thisResult.push(100);
-    //     changeBot((amount -= 100));
-    //   } else if (amount >= 25) {
-    //     thisResult.push(25);
-    //     changeBot((amount -= 25));
-    //   } else if (amount >= 10) {
-    //     thisResult.push(10);
-    //     changeBot((amount -= 10));
-    //   } else if (amount >= 5) {
-    //     thisResult.push(10);
-    //     changeBot((amount -= 5));
-    //   } else if (amount >= 1) {
-    //     thisResult.push(1);
-    //     changeBot((amount -= 1));
-    //   } else if (amount >= 0) {
-    //     return thisResult;
-    //   }
-    // };
+          const addCoin = [ coinValues[index] ];
+          const newArr = result.concat(addCoin);
 
-    // changeBot();
-    // return thisResult;
+          return buildChange(newTotal, index, newArr);
+        } else {
+          return buildChange(amount, (index += 1), result);
+        }
+      } else {
+        const finishedChange = result;
+        console.log("buildChange: ", finishedChange);
+        return finishedChange;
+      }
+    };
+
+    const finalResult = buildChange(startingAmount);
+    console.log("Final: ", finalResult);
+    return finalResult;
   }
 }
 
