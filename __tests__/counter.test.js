bcade5a1eb281138b6765a4b4ac3a4cedc1a13d5
Jeremy Kale Padot <jeremy@padot.us>
2020-04-13 10:14:48 -0700

    add meaningful fail, fixing live share issues
    Co-authored-by: Dusty McCord <dustymccord@gmail.com>
    Co-authored-by: Drake Wilcox <drakewilcox@gmail.com>
    Co-authored-by: Dominique Youmans <46074403+21-Coding@users.noreply.github.com>

--- __tests__/counter.test.js
+++ __tests__/counter.test.js
@@ -1,2 +1,12 @@
-import { Triangle } from "../../src/js/counter.js";
+import { Counter } from "../src/counter.js";
 import { exportAllDeclaration } from "@babel/types";
+
+describe("Counter", () => {
+  test("should return array of change for 100, 25, 25", () => {
+    const counter = new Counter();
+    const changeAmount = 150;
+    const correctAnswerArray = [100, 25, 25];
+    const result = counter.getChange(changeAmount, []);
+    expect(result).toEqual(correctAnswerArray);
+  });
+});
