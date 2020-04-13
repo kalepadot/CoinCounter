760f0f4ebbecc377dc156588529f4b9cf1f3ab83
Jeremy Kale Padot <jeremy@padot.us>
2020-04-13 16:38:36 -0700

    whiteboarding practice

--- src/Counter.js
+++ src/Counter.js
@@ -15,9 +15,29 @@ export class Counter {
           return buildChange(amount, (index += 1), result);
         }
       } else {
-        return result;
+        const finishedChange = result;
+        console.log("buildChange: ", finishedChange);
+        return finishedChange;
       }
     };
-    return buildChange(startingAmount);
+
+    const finalResult = buildChange(startingAmount);
+    console.log("Final: ", finalResult);
+    return finalResult;
   }
 }
+
+// spit ballin:
+
+// const coinTypeAmount = (coinType) => {
+//   return (amount => {
+//     return amount / coinType;
+//   }
+// }
+
+// const quarter = coinTypeAmount(25);
+// const dime = coinTypeAmount(10);
+// const nickel = coinTypeAmount(5);
+// const penny = cointTypeAmount(1);
+
+// const numberOfNickels = nickel(800);
